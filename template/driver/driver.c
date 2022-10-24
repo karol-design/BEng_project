dbe90b8a8f299aed3d284f92ee3f5897c324f1b5
Karol Wojslaw <wojslaw.tech@gmail.com>
2022-10-24 13:07:04 +0200

    Create a template ESP-IDF project

---
+++ template/driver/driver.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "driver.h"
+
+void func(void)
+{
+
+}
---
+++ template/main/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "esp_log.h"
+
+#define TAG "Main"
+
+void app_main(void)
+{
+    LOGI(TAG, "This is a message from the main.c");
+}
