a42c1bd6e8c2223941a8b6589e76dd9a364bd31b
Karol Wojslaw <wojslaw.tech@gmail.com>
2022-10-26 15:07:12 +0200

    Update common_macros.h

--- interrupts/components/common/src/common_macros.h
+++ interrupts/components/common/src/common_macros.h
@@ -1,7 +1,7 @@
 /**
- * @file        common_macros.h
- * @brief       Common macros for use throughout the project
- * @date        2022-10-26
+ * @file    common_macros.h
+ * @brief   Common macros for use throughout the project
+ * @author  Karol Wojslaw (karol.wojslaw@student.manchester.ac.uk)
  */
 
 #pragma once
@@ -9,11 +9,3 @@
 #include <string.h>
 #include "esp_err.h"
 #include "esp_log.h"
-
-#define INTERNAL_TAG "Debug"
-	
-#define VERIFY_SUCCESS(_err_code, ...)															\
-	if ((_err_code) != ESP_OK) {																\
-		ESP_LOGE(INTERNAL_TAG, "Error, code:%d %s", (_err_code), esp_err_to_name(_err_code));	\
-		return _err_code;																		\
-	}
